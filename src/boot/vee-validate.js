2f61b52499d1e81a6a08a54fd3ef87ccbc410ee4
endropie <hendrowibowo@msn.com>
2019-09-24 15:57:33 +0700

    add extend rule vee-validate 'phone'

--- src/boot/vee-validate.js
+++ src/boot/vee-validate.js
@@ -4,6 +4,12 @@ VeeValidate.Validator.extend('eq', (value, [paramValue]) => {
   return value === paramValue
 }, { hasTarget: true, computesRequired: true })
 
+VeeValidate.Validator.extend('phone', (value) => {
+  // eslint-disable-next-line camelcase
+  let phone_regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\\./0-9]*$/g
+  return phone_regex.test(value)
+}, { hasTarget: true, computesRequired: true })
+
 VeeValidate.Validator.extend('lt_value', (value, [paramValue]) => {
   return Number(value) < Number(paramValue)
 }, { hasTarget: true, computesRequired: true })
